8c6b8ef51f71a0ff8e0d254c2289786ccea0cfea
DevJoaoPeu <joaocanaldroid@gmail.com>
2026-04-11 15:33:31 -0300

    feat: add validation from dto

--- order-service/src/order/dto/create-order.dto.ts
+++ order-service/src/order/dto/create-order.dto.ts
@@ -1,26 +1,57 @@
+import { IsArray, IsEnum, IsInt, IsNotEmpty, IsNumber, IsString, Min, ValidateNested } from 'class-validator';
 import { PaymentMethodEnum } from '../enuns/payment-method.enum';
+import { Type } from 'class-transformer';
 
 class ItemDto {
+    @IsNotEmpty()
+    @IsString()
     productId: string;
+
+    @IsNotEmpty()
+    @IsInt()
+    @Min(1)
     quantity: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Min(0)
     unitPrice: number;
 }
 
 class PaymentDto {
+    @IsNotEmpty()
+    @IsEnum(PaymentMethodEnum)
     method: PaymentMethodEnum;
+
+    @IsNotEmpty()
+    @IsNumber()
     installment: number;
 }
 
 class DeliveryDto {
+    @IsNotEmpty()
+    @IsString()
     addressId: string;
 }
 
 export class CreateOrderDto {
+    @IsNotEmpty()
+    @IsString()
     customerId: string;
 
+    @IsNotEmpty()
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => ItemDto)
     itens: ItemDto[];
-    
+
+    @IsNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => PaymentDto)
     payment: PaymentDto
 
+    @IsNotEmpty()
+    @ValidateNested({ each: true })
+    @Type(() => DeliveryDto)
     delivery: DeliveryDto;
 }
