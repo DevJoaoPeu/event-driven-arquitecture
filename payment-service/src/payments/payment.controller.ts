da2150f0696246bd47298c6a93b6e1b1e882a07c
DevJoaoPeu <joaocanaldroid@gmail.com>
2026-04-12 17:58:58 -0300

    feat: add event pattern

--- payment-service/src/payments/payment.controller.ts
+++ payment-service/src/payments/payment.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { PaymentService } from './payment.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class PaymentController {
-  constructor(private readonly paymentService: PaymentService) {}
 
-  @Get()
-  save() {
-    return this.paymentService.save();
+  @EventPattern('order.created')
+  payment(order: any) {
+    console.log(order)
   }
 }
