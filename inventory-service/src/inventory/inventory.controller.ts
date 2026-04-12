c66ee82c659b2ba9c130740e62118fda88e25318
DevJoaoPeu <joaocanaldroid@gmail.com>
2026-04-12 18:58:17 -0300

    feat: add eventPattern

--- inventory-service/src/inventory/inventory.controller.ts
+++ inventory-service/src/inventory/inventory.controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class InventoryController {
-  inventory() {}
+
+  @EventPattern('order.created')
+  inventory(order: any) {
+    console.log(order)
+  }
 }
