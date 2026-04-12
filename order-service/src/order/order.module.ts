b241c781d51c35e4d88ab3a6451e430b3b2c2118
DevJoaoPeu <joaocanaldroid@gmail.com>
2026-04-12 10:41:45 -0300

    feat: add config-service

--- order-service/src/order/order.module.ts
+++ order-service/src/order/order.module.ts
@@ -2,20 +2,25 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
-        ClientsModule.register([
+        ClientsModule.registerAsync([
             {
                 name: "RABBITMQ_SERVICE",
-                transport: Transport.RMQ,
-                options: {
-                    urls: ["amqp://admin:admin@localhost:5672"],
-                    queue: "order_queue",
-                    queueOptions: {
-                        durable: true
+                imports: [ConfigModule],
+                useFactory: ((configService: ConfigService) => ({
+                    transport: Transport.RMQ,
+                    options: {
+                        urls: [configService.getOrThrow<string>('RABBITMQ_URL')],
+                        queue: 'orders_queue',
+                        queueOptions: {
+                            durable: true
+                        }
                     }
-                }
+                })),
+                inject: [ConfigService]
             }]
         )
     ],
